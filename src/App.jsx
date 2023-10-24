a7dae3eb69fda25fb8886cf77e3e1f9b44ce7886
Shibly Nomany <nomanyshibly20@gmail.com>
2023-10-24 21:51:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Home from "./assets/Pages/Home";
+// import { Helmet } from "react-helmet";
+// import {FaBeer} from 'react-icons/fa';
+// import { AiFillAmazonSquare } from "react-icons/ai";
+ 
+ 
+
+const App = () => {
+  return (
+    <div>
+  
+
+          {/* react helmet for meta titel  */}
+{/* 
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>test one</title>
+          <link rel="canonical" href="http://mysite.com/example" />         
+        </Helmet>
+      </div> */}
+
+
+      {/* <BrowserRouter> */}
+        {/* Sobgulate J Section Gula Thakbe like {Navbar} */}
+
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes> */}
+
+        {/* Sobgulate J Section Gula Thakbe like {Footer} */}
+      {/* </BrowserRouter> */}
+    </div>
+  );
+};
+
+export default App;
